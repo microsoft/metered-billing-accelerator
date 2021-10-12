c50d5317cddb9c91158c7aaa3cefc7414017b288
Paolo Salvatori <leprino@hotmail.com>
2021-10-12 20:10:57 +0200

    Added ARM template  and Bash script
    I created a new arm folder where you can find:
    - ARM template
    - ARM parameters file
    - Bash script to deploy the ARM template
    Make sure to edit the names in the parameters file before deploying the ARM template.

---
+++ arm/scripts/deploy.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+# Variables
+resourceGroupName="HackathonRG"
+location="WestEurope"
+deploy=1
+
+# ARM template and parameters files
+template="../templates/azuredeploy.json"
+parameters="../templates/azuredeploy.parameters.json"
+
+# SubscriptionId of the current subscription
+subscriptionId=$(az account show --query id --output tsv)
+subscriptionName=$(az account show --query name --output tsv)
+
+# Check if the resource group already exists
+createResourceGroup() {
+    local resourceGroupName=$1
+    local location=$2
+
+    # Parameters validation
+    if [[ -z $resourceGroupName ]]; then
+        echo "The resource group name parameter cannot be null"
+        exit
+    fi
+
+    if [[ -z $location ]]; then
+        echo "The location parameter cannot be null"
+        exit
+    fi
+
+    echo "Checking if [$resourceGroupName] resource group actually exists in the [$subscriptionName] subscription..."
+
+    if ! az group show --name "$resourceGroupName" &>/dev/null; then
+        echo "No [$resourceGroupName] resource group actually exists in the [$subscriptionName] subscription"
+        echo "Creating [$resourceGroupName] resource group in the [$subscriptionName] subscription..."
+
+        # Create the resource group
+        if az group create --name "$resourceGroupName" --location "$location" 1>/dev/null; then
+            echo "[$resourceGroupName] resource group successfully created in the [$subscriptionName] subscription"
+        else
+            echo "Failed to create [$resourceGroupName] resource group in the [$subscriptionName] subscription"
+            exit
+        fi
+    else
+        echo "[$resourceGroupName] resource group already exists in the [$subscriptionName] subscription"
+    fi
+}
+
+# Validate the ARM template
+validateTemplate() {
+    local resourceGroupName=$1
+    local template=$2
+    local parameters=$3
+    local arguments=$4
+
+    # Parameters validation
+    if [[ -z $resourceGroupName ]]; then
+        echo "The resource group name parameter cannot be null"
+    fi
+
+    if [[ -z $template ]]; then
+        echo "The template parameter cannot be null"
+    fi
+
+    if [[ -z $parameters ]]; then
+        echo "The parameters parameter cannot be null"
+    fi
+
+    echo "Validating [$template] ARM template..."
+
+    if [[ -z $arguments ]]; then
+        error=$(az deployment group validate \
+            --resource-group "$resourceGroupName" \
+            --template-file "$template" \
+            --parameters "$parameters" \
+            --query error \
+            --output json)
+    else
+        error=$(az deployment group validate \
+            --resource-group "$resourceGroupName" \
+            --template-file "$template" \
+            --parameters "$parameters" \
+            --arguments $arguments \
+            --query error \
+            --output json)
+    fi
+
+    if [[ -z $error ]]; then
+        echo "[$template] ARM template successfully validated"
+    else
+        echo "Failed to validate the [$template] ARM template"
+        echo "$error"
+        exit 1
+    fi
+}
+
+# Deploy ARM template
+deployTemplate() {
+    local resourceGroupName=$1
+    local template=$2
+    local parameters=$3
+    local arguments=$4
+
+    # Parameters validation
+    if [[ -z $resourceGroupName ]]; then
+        echo "The resource group name parameter cannot be null"
+        exit
+    fi
+
+    if [[ -z $template ]]; then
+        echo "The template parameter cannot be null"
+        exit
+    fi
+
+    if [[ -z $parameters ]]; then
+        echo "The parameters parameter cannot be null"
+        exit
+    fi
+
+    if [ $deploy != 1 ]; then
+        return
+    fi
+
+    # Deploy the ARM template
+    echo "Deploying [$template$] ARM template..."
+
+    if [[ -z $arguments ]]; then
+         az deployment group create \
+            --resource-group $resourceGroupName \
+            --template-file $template \
+            --parameters $parameters 1>/dev/null
+    else
+         az deployment group create \
+            --resource-group $resourceGroupName \
+            --template-file $template \
+            --parameters $parameters \
+            --parameters $arguments 1>/dev/null
+    fi
+
+    if [[ $? == 0 ]]; then
+        echo "[$template$] ARM template successfully provisioned"
+    else
+        echo "Failed to provision the [$template$] ARM template"
+        exit -1
+    fi
+}
+
+# Create Resource Group
+createResourceGroup \
+    "$resourceGroupName" \
+     "$location"
+
+# Validate ARM Template
+validateTemplate \
+    "$resourceGroupName" \
+    "$template" \
+    "$parameters"
+
+# Deploy ARM Template
+deployTemplate \
+    "$resourceGroupName" \
+    "$template" \
+    "$parameters"
