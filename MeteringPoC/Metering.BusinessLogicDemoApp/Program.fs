911b0dd2496d563c95792a0fa8d827781eb4d960
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-10-13 13:31:45 +0200

    Further refactoring

--- MeteringPoC/Metering.BusinessLogicDemoApp/Program.fs
+++ MeteringPoC/Metering.BusinessLogicDemoApp/Program.fs
@@ -12,96 +12,103 @@ open System.Globalization
 // - Countdown for "included widgets" in current billing period
 // - Once "included widgets" for current billing period are consumed, start counting hourly
 
-let parseUsageEvent (s: string) : UsageEvent option =
-       let parseDate (p: string) : DateTime =
-           DateTime.ParseExact(p, [|"yyyy-MM-dd--HH-mm-ss"|], CultureInfo.CurrentCulture, DateTimeStyles.AssumeUniversal)
+let parsePlans planStrings =
+    let parseBillingDimension (s: string) : PlanID * BillingDimension =
+        let parseQuantity(p: string) : IncludedQuantityMonthly option =
+            UInt64.Parse(p)
+            |> Some
 
-       let parseProps (p: string) =
-           p.Split([|','|])
-           |> Array.toList
-           |> List.map (fun x -> x.Split([|'='|]))
-           |> List.map Array.toList
-           |> List.filter (fun l -> l.Length = 2)
-           |> List.map (function 
-               | [k;v] -> (k.Trim(), v.Trim())
-               | _ -> failwith "cannot happen")
-           |> Map.ofList
-           |> Some
+        s.Split([|'|'|], 5)
+        |> Array.toList
+        |> List.map (fun s -> s.Trim())
+        |> function
+            | [planId; dimensionId; name; unitOfMeasure; includedQuantity] -> 
+                (planId, {
+                    DimensionIdentifier = dimensionId
+                    DimensionName = name
+                    UnitOfMeasure = unitOfMeasure
+                    IncludedQuantityMonthly = includedQuantity |> parseQuantity
+                })
+            | [planId; dimensionId; name; unitOfMeasure] -> 
+                (planId, {
+                    DimensionIdentifier = dimensionId
+                    DimensionName = name
+                    UnitOfMeasure = unitOfMeasure
+                    IncludedQuantityMonthly = None
+                })
+            | _ -> failwith "parsing error"
 
-       s.Split([|'|'|], 5)
-       |> Array.toList
-       |> List.map (fun s -> s.Trim())
-       |> function
-           | [datestr; plan; name; amountstr; props] -> 
-               Some {
-                   PlanID = plan
-                   Dimension = name
-                   Timestamp = datestr |> parseDate
-                   Quantity = amountstr |> UInt64.Parse
-                   Properties = props |> parseProps
-               }
-           | [datestr; plan; name; amountstr] -> 
-               Some {
-                   PlanID = plan
-                   Dimension = name
-                   Timestamp = datestr |> parseDate
-                   Quantity = amountstr |> UInt64.Parse
-                   Properties = None
-               }
-           | _ -> None
+    planStrings
+    |> Seq.map parseBillingDimension
+    |> Seq.groupBy(fun (plan, _) -> plan)
+    |> Seq.map(fun (plan, elems) -> (plan, (elems |> Seq.map(fun (p, e) -> e) ) ))
+    |> Seq.map(fun (planid, billingDims) -> { Id = planid; BillingDimensions = billingDims })
 
-let parseBillingDimension (s: string) : BillingDimension =
-    let parseQuantity(p: string) : IncludedQuantityMonthly option =
-        UInt64.Parse(p)
-        |> Some
+let parseUsageEvents events =
+    let parseUsageEvent (s: string) : UsageEvent option =
+        let parseDate (p: string) : DateTime =
+            DateTime.ParseExact(p, [|"yyyy-MM-dd--HH-mm-ss"|], CultureInfo.CurrentCulture, DateTimeStyles.AssumeUniversal)
 
-    s.Split([|'|'|], 4)
-    |> Array.toList
-    |> List.map (fun s -> s.Trim())
-    |> function
-        | [identifier; name; unitOfMeasure; includedQuantity] -> 
-            {
-                DimensionIdentifier = identifier
-                DimensionName = name
-                UnitOfMeasure = unitOfMeasure
-                IncludedQuantityMonthly = includedQuantity |> parseQuantity
-            }
-        | [identifier; name; unitOfMeasure] -> 
-            {
-                DimensionIdentifier = identifier
-                DimensionName = name
-                UnitOfMeasure = unitOfMeasure
-                IncludedQuantityMonthly = None
-            }
-        | _ -> failwith "parsing error"
+        let parseProps (p: string) =
+            p.Split([|','|])
+            |> Array.toList
+            |> List.map (fun x -> x.Split([|'='|]))
+            |> List.map Array.toList
+            |> List.filter (fun l -> l.Length = 2)
+            |> List.map (function 
+                | [k;v] -> (k.Trim(), v.Trim())
+                | _ -> failwith "cannot happen")
+            |> Map.ofList
+            |> Some
+
+        s.Split([|'|'|], 5)
+        |> Array.toList
+        |> List.map (fun s -> s.Trim())
+        |> function
+            | [datestr; plan; name; amountstr; props] -> 
+                Some {
+                    PlanID = plan
+                    Dimension = name
+                    Timestamp = datestr |> parseDate
+                    Quantity = amountstr |> UInt64.Parse
+                    Properties = props |> parseProps
+                }
+            | [datestr; plan; name; amountstr] -> 
+                Some {
+                    PlanID = plan
+                    Dimension = name
+                    Timestamp = datestr |> parseDate
+                    Quantity = amountstr |> UInt64.Parse
+                    Properties = None
+                }
+            | _ -> None
+    events
+    |> List.map parseUsageEvent
+    |> List.choose id
 
 [<EntryPoint>]
 let main argv =
-    let plan = 
+    let plans = 
         [ 
-            "MachineLearningJob   | An expensive machine learning job | machine learning jobs | 10"
-            "EMailCampaign        | An e-mail sent for campaign usage | e-mails               | 250000"
-            "nodecharge           | Per Node Connected                | node/hour"
-            "cpucharge            | Per CPU urage                     | cpu/hour"
-            "datasourcecharge     | Per DataSource Integration        | ds/hour"
-            "messagecharge        | Per Message Transmitted           | message/hour"
+            "plan1 | nodecharge         | Per Node Connected                | node/hour"
+            "plan1 | cpucharge          | Per CPU urage                     | cpu/hour"
+            "plan1 | datasourcecharge   | Per DataSource Integration        | ds/hour"
+            "plan1 | messagecharge      | Per Message Transmitted           | message/hour"
+            "plan2 | MachineLearningJob | An expensive machine learning job | machine learning jobs | 10"
+            "plan2 | EMailCampaign      | An e-mail sent for campaign usage | e-mails               | 250000"
         ]
-        |> List.map parseBillingDimension
-        |> (fun bd -> { Id = "plan1"; BillingDimensions = bd })
-
+        
     let usageEvents = 
         [
-            "2021-10-13--14-12-02 | plan1 | MachineLearningJob |   1 | Department=Data Science, Project ID=Skunkworks vNext"
-            "2021-10-13--15-12-02 | plan1 | MachineLearningJob |   2                                                       "
-            "2021-10-13--15-13-02 | plan1 | EMailCampaign      | 300 | Email Campaign=User retention, Department=Marketing "
+            "2021-10-13--14-12-02 | plan2 | MachineLearningJob |   1 | Department=Data Science, Project ID=Skunkworks vNext"
+            "2021-10-13--15-12-02 | plan2 | MachineLearningJob |   2                                                       "
+            "2021-10-13--15-13-02 | plan2 | EMailCampaign      | 300 | Email Campaign=User retention, Department=Marketing "
         ]
-        |> List.map parseUsageEvent
-        |> List.choose id
-
+        
     let oldBalance  = {
-        Plans = [ plan ]
+        Plans = plans |> parsePlans
         InitialPurchase = {
-            PlanId = plan.Id
+            PlanId = "plan2"
             PurchaseTimestamp = DateTime.UtcNow.Subtract(TimeSpan.FromHours(26.0)) }
         CurrentCredits =
             [
@@ -112,6 +119,7 @@ let main argv =
 
     let newBalance =
         usageEvents
+        |> parseUsageEvents
         |> BusinessLogic.applyUsageEvents oldBalance 
 
     //printfn "plan %A" plan
