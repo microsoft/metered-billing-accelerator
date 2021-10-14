bf7c3e89b7e2ce44fd40416db18ac0863ae19439
code4clouds <julio.colon@microsoft.com>
2021-10-14 16:41:34 -0400

    Added Library

---
+++ src/metering-billing-accelerator/metering-billing-accelerator-lib/model/MeterRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteringAcceleratorLib.model
+{
+    public class MeterRecord
+    {
+        public string planId;
+        public string tags;
+        public int units;
+        public string dimensionId;
+        public string marketplaceSubsriptionId;
+    }
+}
