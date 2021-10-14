ac8dba3c04fdcbf865c9702ab32cc1ef5ebd9098
Julio Colon <julio.colon@microsoft.com>
2021-10-14 19:13:27 -0400

    Event Hub Library Projects (#4)
    Added the CLI and Library to the Solution

---
+++ src/metering-billing-accelerator/metering-billing-accelerator-cli/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MeteringAcceleratorLib;
+
+namespace EHMeterCLI
+{
+    class Program
+    {
+        /// <summary>
+        /// This is a CLI to test the metering library.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("EVENT_HUB_CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString)) //verify there is a connection string
+            {
+                Console.Error.WriteLine("Can't find EVENT_HUB_CONNECTION_STRING environmantal variable.");
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine("Setting up emitter...");
+            var meterLib = new MeteringAcceleratorLib.MeteringAcceleratorLib(connectionString);  // This uses the defail EventHub name: meter-billing-accelerator      
+
+            try // emit one single record.
+            {
+                await meterLib.EmitAsync("subscription1", "plan1", "dimension1", 1, "");
+                Console.WriteLine("One meter record submitted.");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Possible error the EventHub name doesnt exist.  Default is: meter-billing-accelerator");
+                Console.Error.WriteLine(e);
+            }
+
+            //create a set of records 
+            List<MeteringAcceleratorLib.model.MeterRecord> meterRecords = new List<MeteringAcceleratorLib.model.MeterRecord>();
+            var meterRecord0 = new MeteringAcceleratorLib.model.MeterRecord()
+            {
+                marketplaceSubsriptionId = "subscription2",
+                planId = "plan2",
+                dimensionId = "dim1",
+                tags = "",
+                units = 2
+            };
+            var meterRecord1 = new MeteringAcceleratorLib.model.MeterRecord()
+            {
+                marketplaceSubsriptionId = "subscription2",
+                planId = "plan2",
+                dimensionId = "dim2",
+                tags = "",
+                units = 30
+            };
+            meterRecords.Add(meterRecord0);
+            meterRecords.Add(meterRecord1);
+
+            try  //emy all records created in previous step.
+            {
+                await meterLib.EmitMeterBatchAsync(meterRecords);
+
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Possible error the EventHub name doesnt exist.  Default is: meter-billing-accelerator");
+                Console.Error.WriteLine(e);
+            }
+
+        }
+    }
+}
---
+++ src/metering-billing-accelerator/metering-billing-accelerator-lib/model/MeterRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeteringAcceleratorLib.model
+{
+    /// <summary>
+    /// This is the model to submit records to the event hub to be process an emited to the Azure Marketplace
+    /// </summary>
+    public class MeterRecord
+    {
+        public string marketplaceSubsriptionId;
+        public string planId;
+        public string dimensionId;
+        public int units;
+        public string tags;
+    }
+}
