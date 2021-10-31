90708da185044d34012f1dc837b221274f8e2429
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-10-31 12:30:55 +0100

    client SDK

--- src/MeteringPoC/Metering.ClientSDK/Client.cs
+++ src/MeteringPoC/Metering.ClientSDK/Client.cs
@@ -9,15 +9,52 @@
 
     public class Client
     {
-        public async Task SubmitAsync(string dimension, int unit)
+
+        public async Task SubmitManagedAppIntegerAsync(string meterName, ulong unit)
         {
             await Task.Delay(1);
 
             InternalUsageEvent _ = new(
                 scope: SubscriptionType.ManagedApp,
-                timestamp: new ZonedDateTime(SystemClock.Instance.GetCurrentInstant(), DateTimeZone.Utc), 
-                meterName: "meter1",
-                quantity: QuantityModule.createInt(2),
+                timestamp: new ZonedDateTime(SystemClock.Instance.GetCurrentInstant(), DateTimeZone.Utc),
+                meterName: meterName,
+                quantity: QuantityModule.createInt(unit),
+                properties: FSharpOption<FSharpMap<string, string>>.None);
+        }
+
+        public async Task SubmitManagedAppFloatAsync(string meterName, decimal unit)
+        {
+            await Task.Delay(1);
+
+            InternalUsageEvent _ = new(
+                scope: SubscriptionType.ManagedApp,
+                timestamp: new ZonedDateTime(SystemClock.Instance.GetCurrentInstant(), DateTimeZone.Utc),
+                meterName: meterName,
+                quantity: QuantityModule.createFloat(unit),
+                properties: FSharpOption<FSharpMap<string, string>>.None);
+        }
+
+        public async Task SubmitSaasIntegerAsync(string saasId, string meterName, ulong unit)
+        {
+            await Task.Delay(1);
+
+            InternalUsageEvent _ = new(
+                scope: SubscriptionType.NewSaaSSubscription(SaaSSubscriptionIDModule.create(saasId)),
+                timestamp: new ZonedDateTime(SystemClock.Instance.GetCurrentInstant(), DateTimeZone.Utc),
+                meterName: meterName,
+                quantity: QuantityModule.createInt(unit),
+                properties: FSharpOption<FSharpMap<string, string>>.None);
+        }
+
+        public async Task SubmitSaasFloatAsync(string saasId, string meterName, decimal unit)
+        {
+            await Task.Delay(1);
+
+            InternalUsageEvent _ = new(
+                scope: SubscriptionType.NewSaaSSubscription(SaaSSubscriptionIDModule.create(saasId)),
+                timestamp: new ZonedDateTime(SystemClock.Instance.GetCurrentInstant(), DateTimeZone.Utc),
+                meterName: meterName,
+                quantity: QuantityModule.createFloat(unit),
                 properties: FSharpOption<FSharpMap<string, string>>.None);
         }
     }
