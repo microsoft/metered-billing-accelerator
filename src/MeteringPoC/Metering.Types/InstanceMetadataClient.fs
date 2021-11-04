3dd3d189fceb82a216f417961508df6ac85c8471
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-11-04 16:10:11 +0100

    Instance Metadata Service

--- src/MeteringPoC/Metering.Types/InstanceMetadataClient.fs
+++ src/MeteringPoC/Metering.Types/InstanceMetadataClient.fs
@@ -1,33 +1,76 @@
 ﻿namespace Metering.Types
 
-open System.IO
+open System
 open System.Net.Http
+open System.Net.Http.Json
 open System.Web
-open Newtonsoft.Json
-open FSharp.Control.Tasks
+open Microsoft.FSharp.Control
 
-type TokenDefinition = { access_token: string }
 //type ResourceGroupDefinition = { ManagedBy: string }
 
 module InstanceMetadataClient = 
-    let demo =
-        task {
-            let msiEndpoint = "http://169.254.169.254/metadata/identity/oauth2/token"
-            let resource = "https://management.core.windows.net/"
-            let apiVersion = "2017-09-01"
-            let url = $"{msiEndpoint}?resource={HttpUtility.UrlEncode(resource)}&api-version={apiVersion}"
+    type TokenResponse = { access_token: string }
+    
+    let get_access_token : (string -> string) =
+        let getMSIConfigFromEnvironment =
+            let env s = 
+                match s |> Environment.GetEnvironmentVariable with
+                | v when String.IsNullOrWhiteSpace(v) -> None
+                | v -> Some v
 
-            use client = new HttpClient()
-            use request = new HttpRequestMessage(HttpMethod.Get, url)
-            request.Headers.Add("Metadata", "true")
-            // request.Headers.Add("Secret", config.MSISecret)
+            // https://docs.microsoft.com/en-us/azure/app-service/overview-managed-identity?tabs=dotnet#using-the-rest-protocol
+            match (env "IDENTITY_ENDPOINT", env "IDENTITY_HEADER", env "MSI_ENDPOINT", env "MSI_SECRET") with
+            | (None, None, Some msiEndpoint, Some msiSecret) -> (msiEndpoint, Some ("secret", msiSecret), "2017-09-01")
+            | (Some identityEndpoint, Some identityHeader, _, _) -> (identityEndpoint, Some ("X-IDENTITY-HEADER", identityHeader), "2019-08-01")
+            | _ -> ("http://169.254.169.254/metadata/identity/oauth2/token", None, "2019-08-01")
+            
+        let access_token_retriever (resource: string) =
+            task {
+                let (endpoint, headerAndSecret, apiVersion) = getMSIConfigFromEnvironment
+    
+                use client = new HttpClient()
+                client.BaseAddress <- new Uri(endpoint)
+                client.DefaultRequestHeaders.Add("Metadata", "true")
 
-            let! response = client.SendAsync(request)
+                match headerAndSecret with
+                | Some (h, v) -> client.DefaultRequestHeaders.Add(h, v) 
+                | None -> ()
+    
+                let! { access_token = token } =
+                    client.GetFromJsonAsync<TokenResponse>($"?resource={resource |> HttpUtility.UrlEncode}&api-version={apiVersion}")
 
-            let! responseBody = response.Content.ReadAsStringAsync()
-            //let accessToken = JsonConvert.DeserializeObject<TokenDefinition>(responseBody).access_token
+                return token
+            }
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+        
+        access_token_retriever
 
-            return responseBody
-        }
-        |> Async.AwaitTask
-        |> Async.RunSynchronously
+    let create (resource: string) : HttpClient =
+        let client = new HttpClient()
+        client.BaseAddress <- new Uri(resource)
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {get_access_token resource}")
+        client
+
+    let private demo =
+        let inspect header a =
+            if String.IsNullOrEmpty header 
+            then printfn "%s" a
+            else printfn "%s: %s" header a
+            a
+
+        "https://management.azure.com/" 
+        |> get_access_token 
+        |> inspect "access_token"
+        |> ignore
+
+        "https://management.azure.com/"
+        |> create 
+        |> (fun (client : HttpClient) -> 
+            task {
+                return! client.GetStringAsync("/subscriptions?api-version=2020-01-01")
+            }
+            |> Async.AwaitTask
+            |> Async.RunSynchronously)
+        |> inspect "subscriptions"
+        |> ignore
