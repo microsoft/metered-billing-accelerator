d2764c15429bcc26fa26ae9fb68e03f25343e955
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2021-12-09 14:58:19 +0100

    Removed remaining fragments of offset

--- src/MeteringPoC/Metering.Types/Json.fs
+++ src/MeteringPoC/Metering.Types/Json.fs
@@ -44,14 +44,13 @@ module Json =
     module MessagePosition =
         open Metering.Types.EventHub
 
-        let (partitionId, sequenceNumber, partitionTimestamp, offset) = 
-            ("partitionId", "sequenceNumber", "partitionTimestamp", "offset")
+        let (partitionId, sequenceNumber, partitionTimestamp) = 
+            ("partitionId", "sequenceNumber", "partitionTimestamp")
 
         let Encoder (x: MessagePosition) : JsonValue =
             [
                 (partitionId, x.PartitionID |> PartitionID.value |> Encode.string)
                 (sequenceNumber, x.SequenceNumber |> Encode.int64)
-                // (offset, x.Offset |> Encode.int64)
                 (partitionTimestamp, x.PartitionTimestamp |> MeteringDateTime.Encoder)
             ]
             |> Encode.object 
@@ -60,7 +59,6 @@ module Json =
             Decode.object (fun get -> {
                 PartitionID = (get.Required.Field partitionId Decode.string) |> PartitionID.create
                 SequenceNumber = get.Required.Field sequenceNumber Decode.int64
-                // Offset = get.Required.Field offset Decode.int64
                 PartitionTimestamp = get.Required.Field partitionTimestamp MeteringDateTime.Decoder                
             })
 
