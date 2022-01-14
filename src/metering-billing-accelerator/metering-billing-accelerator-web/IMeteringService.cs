159190659fc3cac42bd5fdfea5845b1e08e19e1a
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-01-14 10:10:43 +0100

    Removed old code

--- src/metering-billing-accelerator/metering-billing-accelerator-web/IMeteringService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace metering_billing_accelerator_web
-{
-    internal interface IMeteringService
-    {
-        public Task EmitMeterAsync(string subscriptionId, string planId, string dimensionId, int units, string tags);
-    }
-}
