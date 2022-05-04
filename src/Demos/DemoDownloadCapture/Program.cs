2294227954049343ce244f13f001960d649ee3ce
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-05-04 13:14:18 +0200

    cleanup

--- src/Demos/DemoDownloadCapture/Program.cs
+++ src/Demos/DemoDownloadCapture/Program.cs
@@ -16,23 +16,20 @@ var x = CaptureProcessorCS.ReadAllEvents(
     partitionId: PartitionID.create("2"));
 
 await foreach (MeteringUpdateEvent e in x)
+{ 
+    (e switch
+    {
+        SubscriptionPurchased sp => $"Sub purchased {sp.Item.Subscription.InternalResourceId.ToString()}",
+        MeteringUpdateEvent.UnprocessableMessage um => $"Unprocessable message {um.Item.ToString()}",
+        SubscriptionDeletion d => $"Deleted {d.Item.ToString()}",
+        UsageReported u => $"Usage {u.Item.Timestamp} {u.Item.InternalResourceId} {u.Item.Quantity}",
+        UsageSubmittedToAPI u => $"Reported: {u.Item.Result.ToString()}",
+        _ => $"Something else"
+    })
+    .Print();
+}
+
+static class MyExtensios
 {
-    var msg =
-        e switch
-        {
-            SubscriptionPurchased sp => $"Sub purchased {sp.Item.Subscription.InternalResourceId.ToString()}",
-            MeteringUpdateEvent.UnprocessableMessage um => $"Unprocessable message {um.Item.ToString()}",
-            SubscriptionDeletion d => $"Deleted {d.Item.ToString()}",
-            UsageReported u => $"Usage {u.Item.InternalResourceId} {u.Item.Quantity}",
-            UsageSubmittedToAPI u => $"Reported: {u.Item.Result.ToString()}",
-            _ => $"Something else"
-        };
-
-    Console.WriteLine(msg);
-
-    //var usageSubmitted = e as UsageSubmittedToAPI;
-    //if (usageSubmitted != null)
-    //{
-    //    Console.WriteLine(usageSubmitted.Item.Result);
-    //}
+    internal static void Print(this string s) => Console.Out.WriteLine(s);
 }
