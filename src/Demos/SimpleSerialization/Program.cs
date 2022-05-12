5e123ff39b28bfcb1b91347c8dab5adb37ae1665
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-05-12 18:42:03 +0200

    Small serializerDemo

---
+++ src/Demos/SimpleSerialization/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Metering.BaseTypes;
+
+string meterCollectionAsJson = File.ReadAllText(@"..\..\..\..\..\Metering.Tests\data\state.json");
+MeterCollection meterCollection = Json.fromStr<MeterCollection>(meterCollectionAsJson);
+foreach (KeyValuePair<InternalResourceId, Meter> kvp in meterCollection.MeterCollection)
+{
+    InternalResourceId meterResourceId = kvp.Key;
+    Meter meter = kvp.Value;
+
+    foreach (KeyValuePair<DimensionId, MeterValue> meterKey in meter.CurrentMeterValues.value)
+    {
+        DimensionId dimensionId = meterKey.Key;
+        MeterValue meterValue = meterKey.Value;
+
+        Console.WriteLine($"{meterResourceId}: {dimensionId} - {meterValue}");
+    }
+}
