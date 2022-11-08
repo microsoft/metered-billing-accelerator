3b91c757cf56db0b1a32d1600517a0f719af6814
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-11-08 13:31:34 +0100

    Added skeleton for WaterfallMeteringValue.

---
+++ src/Metering.BaseTypes/WaterfallMeteringValue.fs
@@ -0,0 +1,34 @@
+﻿
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Metering.BaseTypes
+
+type WaterfallDescriptionItem = 
+    { Begin: Quantity
+      Name: string }
+
+type Range =
+    { DimensionId: DimensionId
+      LowerIncluding: Quantity
+      UpperExcluding: Quantity }
+
+type Overage =
+    { DimensionId: DimensionId
+      LowerIncluding: Quantity }
+
+type WaterfallModelRow =  
+  | FreeIncluded of Quantity // For included quantities, no reporting to the metering API
+  | Range of Range
+  | Overage of Overage
+
+type WaterfallDescription =
+  WaterfallDescriptionItem list
+
+type WaterfallModel = 
+  WaterfallModelRow list
+
+type WaterfallMeterValue =
+  { Model: WaterfallModel 
+    Total: Quantity
+    Consumption: Map<DimensionId, Quantity> }
