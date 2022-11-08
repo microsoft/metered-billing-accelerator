4b4f14ad8970b6203f9bc32149a3d95495159a36
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-11-08 15:52:13 +0100

    Moved cheese around

--- src/Metering.BaseTypes/SimpleConsumptionBillingDimension.fs
+++ src/Metering.BaseTypes/SimpleConsumptionBillingDimension.fs
@@ -24,33 +24,3 @@ module SimpleConsumptionBillingDimension =
         |> List.map(fun x -> (x.DimensionId, x.IncludedQuantity |> toIncluded))
         |> Map.ofSeq
         |> CurrentMeterValues.create
-
-//type WaterfallRange =
-//    { DimensionId: DimensionId 
-//      LowerIncluding: Quantity
-//      UpperExcluding: Quantity }
-
-//type WaterfallEntry =
-//    | Included of Quantity
-//    | Below of WaterfallRange
-//    | Overage of DimensionId
-
-//module WaterfallEntry =
-//    let included (quantity: uint) =
-//        quantity |> Quantity.create |> Included
-
-//    let below dimension (lowerIncluding:uint) (upperExcluding: uint) =
-//        { DimensionId = dimension |> DimensionId.create
-//          LowerIncluding = lowerIncluding |> Quantity.create 
-//          UpperExcluding = upperExcluding |> Quantity.create } 
-//        |> Below
-//    let overage dimension =
-//        dimension |> DimensionId.create |> Overage
-
-//type WaterfallBilling = 
-//    { /// Application-internal name of the meter / billing dimension. 
-//      MeterName: ApplicationInternalMeterName
-      
-//      WaterfallDimensions: WaterfallEntry list
-
-//      IncludedQuantity: Quantity }
