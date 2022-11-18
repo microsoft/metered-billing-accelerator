559ac01cc0753b85cfd7da30909ebc9f4177cf91
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-11-18 16:46:18 +0100

    Consolidated types

---
+++ src/Metering.BaseTypes/StringTypes.fs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Metering.BaseTypes
+
+/// The ID of the plan (as defined in partner center)
+type PlanId = 
+    private | Value of string
+
+    member this.value
+        with get() =
+            let v (Value x) = x
+            this |> v
+
+    static member create x = (Value x)
+
+/// The immutable dimension identifier referenced while emitting usage events (as defined in partner center).
+type DimensionId =
+    private | Value of string
+
+    member this.value
+        with get() =
+            let v (Value x) = x
+            this |> v
+
+    static member create x = (Value x)
+
+/// A meter name used between app and aggregator
+type ApplicationInternalMeterName = 
+    private | Value of string 
+
+    member this.value
+        with get() =
+            let v (Value x) = x
+            this |> v
+
+    static member create x = (Value x)
