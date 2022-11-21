4d848bd923e4e26444ec148d92a49a5433f7fd9d
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-11-21 16:47:10 +0100

    Excluded the src/Demos/SaaSIntegration/src/PublisherPortal for the time being

--- src/Demos/SaaSIntegration/src/PublisherPortal/ViewModels/Meter/DimensionTotalModel.cs
+++ src/Demos/SaaSIntegration/src/PublisherPortal/ViewModels/Meter/DimensionTotalModel.cs
@@ -1,10 +1,28 @@
-﻿namespace PublisherPortal.ViewModels.Meter
-{
-    public class DimensionTotalModel
-    {
-        public string DimensionName { get; set; }
-        public decimal ConsumedDimensionQuantity { get; set; }
-        public decimal IncludedDimensionQuantity { get; set; }
-        public decimal ToBeProcess { get; set; }
-    }
-}
+﻿namespace PublisherPortal.ViewModels.Meter;
+
+using System.Collections.Generic;
+
+public record DimensionTotalModel(
+    string  DimensionName,
+    decimal ConsumedDimensionQuantity,
+    decimal IncludedDimensionQuantity,
+    decimal ToBeProcess);
+
+public record MeterSummaryModel(
+    string SubscriptionId,
+    string DimensionName,
+    decimal ConsumedDimensionQuantity,
+    decimal IncludedDimensionQuantity);
+
+public record ToBeReportedModel(
+    string SubscriptionId,
+    decimal Quantity,
+    string PlanId,
+    string DimensionName,
+    string EffectiveStartTime);
+
+public record PublisherMetersModel(
+   string LastProcessedMessage,
+   List<DimensionTotalModel> CurrentTotal,
+   List<ToBeReportedModel> CurrentToBeReported,
+   List<MeterSummaryModel> CurrentMeterSummary);
