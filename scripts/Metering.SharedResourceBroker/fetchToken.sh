193b904a189a80286eb138d057441e28323bae66
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-11-29 10:57:09 +0100

    Local development scripts

---
+++ scripts/Metering.SharedResourceBroker/fetchToken.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+basedir="/mnt/c/github/chgeuer/metered-billing-accelerator/scripts/Metering.SharedResourceBroker"
+echo "Working in directory ${basedir}"
+
+CONFIG_FILE="${basedir}/config.json"
+
+function get-value { 
+    local key="$1" ;
+    local json ;
+    
+    json="$( cat "${CONFIG_FILE}" )" ;
+    echo "${json}" | jq -r "${key}"
+}
+
+resource="https://storage.azure.com/.default"
+access_token="$(curl \
+    --silent \
+    --request POST \
+    --data-urlencode "response_type=token" \
+    --data-urlencode "grant_type=client_credentials" \
+    --data-urlencode "client_id=$( get-value '.creds.appAppId')" \
+    --data-urlencode "client_secret=$( get-value '.creds.client_secret')" \
+    --data-urlencode "scope=${resource}" \
+    "https://login.microsoftonline.com/$( get-value '.aad.tenantId')/oauth2/v2.0/token" | \
+        jq -r ".access_token")"
+
+echo "${access_token}" | jq -R 'split(".") | .[1] | @base64d | fromjson'
+
+# .creds.appObjectId
+# .creds.appAppId
+# .creds.spObjectId
+# .creds.client_secret
