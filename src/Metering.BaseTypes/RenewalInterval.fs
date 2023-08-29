6007ee5962ac14fd0cce4fcff41c38079ff5a486
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-08-29 10:19:01 +0200

    Initial support for 2 and 3 years SaaS subscriptions

--- src/Metering.BaseTypes/RenewalInterval.fs
+++ src/Metering.BaseTypes/RenewalInterval.fs
@@ -8,14 +8,20 @@ open NodaTime
 type RenewalInterval =
     | Monthly
     | Annually
+    | TwoYears
+    | ThreeYears
 
     member this.Duration
         with get() =
             match this with
             | Monthly -> Period.FromMonths(1)
             | Annually -> Period.FromYears(1)
+            | TwoYears -> Period.FromYears(2)
+            | ThreeYears -> Period.FromYears(3)
 
      member this.add (i: uint) : Period =
         match this with
         | Monthly -> Period.FromMonths(int i)
         | Annually -> Period.FromYears(int i)
+        | TwoYears -> Period.FromYears(2 * (int i))
+        | ThreeYears -> Period.FromYears(3 * (int i))
